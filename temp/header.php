d6632ec8dbb9c6dec32a6a5bd9361298e5bcd991
DeafArkadiy19971 <81534705+DeafArkadiy19971@users.noreply.github.com>
2021-03-29 15:29:00 +0300

    Add files via upload

---
+++ temp/header.php
@@ -0,0 +1,33 @@
+<header>
+                    
+                    <div class="single-item">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-3 col-sm-12">
+                                    <div class="logo">
+                                        <a href="index.php"><img src="img/logo/logo.png" alt="EDUCAT"></a>
+                                    </div>
+                                </div>
+                                <div class="col-md-9">
+                                    <div class="mainmenu-area pull-right">
+                                        <div class="mainmenu hidden-sm hidden-xs">
+                                        <ul id="nav">
+                                                    <li class="current"><a href="TypeKab.php">Вид кабинет</a></li>
+                                                    <li><a href="Premises.php">Помещение</a></li>
+                                                    <li><a href="Kabinet.php">Кабинет</a>
+                                                    <li><a href="Objectovz.php">Оборудование ОВЗ</a>
+                                                    </li>
+                                                    <button itemprop="copy"   class="btn btn-warning"  href="/reverse.php?sp=1">Наличие версии для слабовидящих</button>
+                                                    <button itemprop="copy" type="button" class="btn btn-warning">  Главная </button>
+                                    </div>	
+
+    				
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Mobile Menu Area end -->    
+                </header>
+
+
+
