5254fd3f9e70a92969c76c137241cc2bf4fb54e7
Ch2aa8 <81554133+Ch2aa8@users.noreply.github.com>
2021-03-29 16:12:28 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,96 @@
+<?php
+// =============connection==========
+include 'temp/connection.php';
+// =============head1==========
+include 'temp/head1.php';       
+                // <!-- ==========header=============== -->
+    include 'temp/header.php';
+                    // <!-- ==========navbar=============== -->
+    include 'temp/navbar.php';
+    
+    include 'temp/navbar2.php';
+
+?>
+
+
+                <div class="course-area section-padding bg-white">
+                    <div class="container">
+
+                        <div class="row">
+                        <h1>Руководство. Педагогический (научно-педагогический) состав</h1><br><br><br>
+<!--===============КАТРОЧКА 1 ================-->
+                        <div class="col-md-4 col-sm-6">
+                                <div class="single-item">
+                                    <div class="single-item-image overlay-effect">
+                                        <a href="rukovod.php"><img src="img/r1.jpg" alt=""></a>
+                                    </div>
+                                    <div class="single-item-text">
+                                        <!-- ====itemprop="rucovodstvo"====== -->
+                                        <h4><a href="rukovod.php">Информация о руководителе образовательной организации/заместителях руководителя образовательной организации</a></h4>
+                                        <div class="single-item-text-info">
+                                        </div>
+                                        <div class="single-item-content">
+                                           <div class="single-item-rating">
+                                        </div>
+                                        </div>   
+                                    </div>
+                                    <div class="button-bottom">
+                                        <a href="rukovod.php" class="button-default">Посмотреть</a>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            
+<!--===============КАТРОЧКА 2 ================-->
+                            <div class="col-md-4 col-sm-6">
+                                <div class="single-item">
+                                    <div class="single-item-image overlay-effect">
+                                        <a href="filial.php"><img src="img/f1.jpg" alt=""></a>
+                                    </div>
+                                    <div class="single-item-text">
+
+                                        <h4><a href="filial.php">Информация о руководителях филиалов образовательной организации</a></h4>
+                                        <div class="single-item-text-info">
+                                        </div>
+                                        <div class="single-item-content">
+                                           <div class="single-item-rating">
+                                        </div>
+                                        </div>   
+                                    </div>
+                                    <div class="button-bottom">
+                                        <a href="filial.php" class="button-default">Посмотреть</a>
+                                    </div>
+                                </div>
+                            </div>
+
+
+<!--===============КАТРОЧКА 3 ================-->
+                            <div class="col-md-4 col-sm-6">
+                                <div class="single-item">
+                                    <div class="single-item-image overlay-effect">
+                                        <a href="prepod.php"><img src="img/p1.jpg" alt=""></a>
+                                    </div>
+                                    <div class="single-item-text">
+                                        <h4><a href="prepod.php">Информация о составе педагогических работников образовательной организации</a></h4>
+                                        <div class="single-item-text-info">
+                                        </div>
+                                        <div class="single-item-content">
+                                           <div class="single-item-rating">
+                                        </div>
+                                        </div>   
+                                    </div>
+                                    <div class="button-bottom">
+                                        <a href="prepod.php" class="button-default">Посмотреть</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                               
+                <!--============footer===============  -->
+<?php                
+                include 'temp/footer.php';
+                include 'temp/head2.php';
+?>      
