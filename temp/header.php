82b31b308582974cc68361554b94851c83a248d7
DeafArkadiy19971 <81534705+DeafArkadiy19971@users.noreply.github.com>
2021-03-30 19:03:45 +0300

    Add files via upload

---
+++ temp/header.php
@@ -0,0 +1,20 @@
+<header>
+                    
+                    <div class="single-item">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-3 col-sm-12">
+                                    <div class="logo">
+                                        <a href="index.php"><img src="img/logo/logo.png" alt="EDUCAT"></a>
+                                
+
+    				
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Mobile Menu Area end -->    
+                </header>
+
+
+
